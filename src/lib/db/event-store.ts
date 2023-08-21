1acee3a55c6e0ef308939510894ceb4cbbe23ddc
Kamil Zegier <kamilzegier@gmail.com>
2023-08-21 09:56:05 +0200

    Add extra condition for id calculation

--- src/lib/db/event-store.ts
+++ src/lib/db/event-store.ts
@@ -2,6 +2,7 @@ import {
     IEvent,
     IBaseEvent,
     SEGMENT_UPDATED,
+    FEATURE_IMPORT,
     IEventType,
 } from '../types/events';
 import { LogProvider, Logger } from '../logger';
@@ -158,7 +159,8 @@ class EventStore implements IEventStore {
             .where((builder) =>
                 builder
                     .whereNotNull('feature_name')
-                    .orWhere('type', SEGMENT_UPDATED),
+                    .orWhere('type', SEGMENT_UPDATED)
+                    .orWhere('type', FEATURE_IMPORT),
             )
             .andWhere('id', '>=', largerThan)
             .first();
